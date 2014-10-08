7375bb8313c3dcb6da7ec5cdb86bf10953be1c66
Oliver Zhang <zhangoliver7@gmail.com>
2014-10-08 12:29:45 -0400

    worked on regex dates

--- checkWhen/testWhen.py
+++ checkWhen/testWhen.py
@@ -1,6 +1,5 @@
 import string
 import re
-from bs4 import BeautifulSoup
 
 #DIFFERENT TEXT FILES TO CHECK
 text = open("important_dates.txt",'r').read()
@@ -9,27 +8,51 @@ dates = []
 sortedDates = []
 dictDates = {}
 
+months = ["January","February","March","April","May","June","July","August","September","October","November","December","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+
 def addDict(n):
     if dictNames.has_key(n):
         dictNames[n] = dictNames[n] + 1
     else:
         dictNames[n] = 1
 
-def findMatches(text):
+def findMatches1(text):
 
-    #################FIND MATCHES
+    #################FIND MATCHES BASED ON MONTH, DAY, YEAR FORMAT
     n = re.compile("[A-Z][a-z]+\s[0-3]?[0-9],?\s[0-9]{1,4}")
     #names = n.match(text)
     #print names.group()
     dates = n.findall(text)
     print dates
-
+    
     #################SORT MATCHES
     print "\nFILTERED DATES:"
+    m = re.compile("[A-Z][a-z]+")
     for date in dates:
-        print date
+        month = m.match(date).group()
+        if month in months:
+            sortedDates.append(date)
+    dates = []
+    d = re.compile("\s([0-3]?[0-9]),?")
+    for date in sortedDates:
+        day = d.findall(date)[0]
+        if int(day) < 32:
+            dates.append(date)
+    return dates
+
+def findMatches2(text):
+
+    #############FIND MATCHES BASED ON SLASH / OR DASH - FORMAT
+    n = re.compile("[0-2]?[0-9].[0-3]?[0-9].[0-9]{1,4}")
+    dates = n.findall(text)
+    print dates
+    
                 
 if __name__ == "__main__":
-    findMatches(text)
+    print findMatches1(text)
+    findMatches2(text)
+    """
     print sortedDates
+    print dates
     print dictDates.items()
+    """
